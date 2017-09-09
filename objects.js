ffe087cfb8d349752cc4db9211d654a5be148c82
Antonino Badalamenti <bada.nino95@gmail.com>
2017-09-09 03:18:41 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 playlist = new Object( {})
 playlist.liga = "una vita da mediano"
+updatePlaylist(playlist,"tizianoFerro","111")
 console.log(playlist)
 function updatePlaylist(obj,name,song){
   obj[name]=song
